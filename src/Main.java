22e4a7857851cd9ab91840da2b345ff94c4dd86a
benlapidge <benlapidge@gmail.com>
2021-11-17 18:22:24 +0000

    EX3 now completed - and with Hashmaps & conversion to ArrayList!

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,14 @@ class Main {
         ItemLine line = new ItemLine("coin",2);
         System.out.println("line.getName() returned " + line.getName());
         System.out.println("line.getQuantity() returned " + line.getQuantity());
+
+        //Below used from EX3
+        Entity entity = new Entity('*');
+        System.out.println("entity.getCode = " + entity.getCode());
+        entity.setItemQuantity("coins", 20);
+        entity.setItemQuantity("handbags", 1);
+        entity.setItemQuantity("treacle", 2);
+        entity.setItemQuantity("stain remover", 1);
+        System.out.println(entity.getItemNames());
     }
 }
